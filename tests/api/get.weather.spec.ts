f5ae8355ce6c449549af607f3426b8cd00e4794a
Junhao Hu <junhao.hu@hotmail.com>
2023-07-26 18:10:50 +1000

    Restructure API interface type definition and JsonSchema file

--- tests/api/get.weather.spec.ts
+++ tests/api/get.weather.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from "@playwright/test";
 import { XMLBuilder } from "fast-xml-parser";
 import xmlFormat from "xml-formatter";
 import { generateResponseFile } from "../../utils/generate.response.file";
+import { GetWeatherResponse } from "../../models/types/getWeatherResponse";
 import Ajv, { JSONSchemaType } from "ajv";
 import fs from "fs";
 import dotenv from "dotenv";
@@ -9,51 +10,9 @@ dotenv.config();
 
 const ajv = new Ajv();
 
-interface GetWeatherResponse {
-  location: {
-    name: string;
-    region: string;
-    country: string;
-    lat: number;
-    lon: number;
-    tz_id: string;
-    localtime_epoch: number;
-    localtime: string;
-  };
-  current: {
-    last_updated_epoch: number;
-    last_updated: string;
-    temp_c: number;
-    temp_f: number;
-    is_day: number;
-    condition: {
-      text: string;
-      icon: string;
-      code: number;
-    };
-    wind_mph: number;
-    wind_kph: number;
-    wind_degree: number;
-    wind_dir: string;
-    pressure_mb: number;
-    pressure_in: number;
-    precip_mm: number;
-    precip_in: number;
-    humidity: number;
-    cloud: number;
-    feelslike_c: number;
-    feelslike_f: number;
-    vis_km: number;
-    vis_miles: number;
-    uv: number;
-    gust_mph: number;
-    gust_kph: number;
-  };
-}
-
 const getWeatherResponseSchema: JSONSchemaType<GetWeatherResponse> = JSON.parse(
   fs.readFileSync(
-    `${process.cwd()}/JSONSchema/get.weather.response.schema.json`,
+    `${process.cwd()}/models/schema/get.weather.response.schema.json`,
     "utf-8"
   )
 );
